fbbd2a0cd84f0a0301a0f1361a054c38c51ee6c4
Saean Shin <160099366+brynn0728@users.noreply.github.com>
2025-05-10 00:04:16 -0500

    Updated my name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <BrowserRouter>
             <div className="App">
                 <Helmet>
-                    <title>Saean Shin's portfolio</title>
+                    <title>Brynn Shin's portfolio</title>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
                         rel="stylesheet"
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
                                 Hello, I'm
                             </h2>
                             <h1 className="name animate-on-scroll">
-                                Saean Shin
+                                Brynn Shin
                             </h1>
                             <h2 className="title animate-on-scroll">
                                 A DS Junior at <span>UW-Madison</span>
